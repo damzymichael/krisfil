553e66f156bbc39cc9d85ba21b2e6d1320e38079
damzymichael <105460672+damzymichael@users.noreply.github.com>
2024-02-11 02:39:18 +0100

    Reduced services to six Reduced services description Added navbar

--- src/data.ts
+++ src/data.ts
@@ -1,39 +1,34 @@
-export const ourServices: {heading: string; description: string}[] = [
-  {
-    heading: 'Restyling',
-    description:
-      "Update your wardrobe with our restyling service. Transform outdated or ill-fitting garments into modern, stylish pieces that reflect your current taste and personality. Whether it's adjusting silhouettes, updating trims, or adding embellishments, we'll work with you to achieve the look you desire"
-  },
+interface OurServices {
+  heading: string;
+  description: string;
+}
+
+export const ourServices: OurServices[] = [
   {
     heading: 'Custom Tailoring',
     description:
-      'Create garments tailored specifically to your measurements and preferences. From suits and shirts to dresses and outerwear, our custom tailoring service ensures a perfect fit and personalized design for every occasion.'
+      'Precision-crafted garments tailored to your measurements and style.'
   },
   {
     heading: 'Alterations',
-    description:
-      "Whether it's hemming, taking in or letting out, our expert tailors specialize in alterations to make your clothing fit like a dream. No matter the complexity of the alteration, we pay attention to every detail to ensure a flawless finish."
+    description: 'Fine-tuning for the perfect fit'
   },
   {
     heading: 'Repairs',
-    description:
-      'Extend the life of your favorite garments with our repair services. From fixing seams and buttons to replacing zippers and patches, our skilled team can breathe new life into your clothing, saving you time and money.'
+    description: 'Meticulous restoration to extend garment life'
   },
   {
     heading: 'Consultation and Fittings',
-    description:
-      'Our experienced team provides personalized consultation and fittings to ensure that every garment meets your expectations. We take the time to understand your needs, preferences, and style goals, guiding you through the process to achieve the perfect fit and look.'
+    description: 'Personalized attention for your unique needs'
   },
 
   {
     heading: 'Uniform Tailoring',
-    description:
-      "Ensure a professional and polished look for your team with our uniform tailoring service. From corporate attire to hospitality uniforms, we can customize garments to meet your company's branding and sizing requirements"
+    description: 'Professional attire tailored to your brand'
   },
   {
     heading: 'Bridal and Formal Wear',
-    description:
-      'Make your special day even more memorable with our bridal and formal wear services. From bridal gown alterations to suit fittings for the groom and groomsmen, we ensure that you look your absolute best on your big day.'
+    description: 'Bespoke attire for your special occasions'
   }
 ];
 
